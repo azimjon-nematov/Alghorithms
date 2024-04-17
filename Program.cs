5726ac0740bcf5620aa5443c508be939998f9127
Azimjon-Nematov <99798110+azimjon-nematov@users.noreply.github.com>
2024-04-17 09:16:05 +0500

    fix

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 string connectionString = builder.Configuration["ConnectionString"];
 //builder.Services.AddTransient<IRepository, Repository>(provider => new Repository(connectionString));
-builder.Services.AddTransient<ITopicRepository, TopicRepository>(provider => new TopicRepository(connectionString));
-builder.Services.AddTransient<IContentRepository, ContentRepository>(provider => new ContentRepository(connectionString));
+builder.Services.AddTransient<ITopicRepository, TopicRepository>(
+    provider => new TopicRepository(connectionString));
+builder.Services.AddTransient<IContentRepository, ContentRepository>(
+    provider => new ContentRepository(connectionString));
 
 builder.Services.AddControllersWithViews(); // добавляем сервисы MVC
 
