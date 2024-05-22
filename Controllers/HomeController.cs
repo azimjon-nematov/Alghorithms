e9a81ad6cb8fdffbcb059df8f3ca8827fac42d48
Azimjon-Nematov <99798110+azimjon-nematov@users.noreply.github.com>
2024-05-22 13:30:54 +0500

    fix

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Alghorithms.Controllers
 {
@@ -14,6 +15,7 @@ namespace Alghorithms.Controllers
             return View();
         }
 
+        //[Authorize(Roles = "True")]
         public IActionResult InDevelopment()
         {
             return View();
