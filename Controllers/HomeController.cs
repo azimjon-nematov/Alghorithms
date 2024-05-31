316c34e83afef57b3bb5875e34cf4c8158ef4e56
Azimjon-Nematov <99798110+azimjon-nematov@users.noreply.github.com>
2024-05-31 16:20:17 +0500

    Contact done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,5 +20,10 @@ namespace Alghorithms.Controllers
         {
             return View();
         }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
     }
 }
